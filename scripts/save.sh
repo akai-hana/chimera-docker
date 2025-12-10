7c12a6d9cbf0e3c1e777d5575fb4bf2fd8741c34
Matías Fernando Moya Mendoza <a251366mm@correu.escoladeltreball.org>
2025-12-10 08:46:55 +0100

    automated sync

--- scripts/save.sh
+++ scripts/save.sh
@@ -1,10 +1,10 @@
 #!/bin/dash
+
 docker commit chimera-akai chimera-akai:latest
 # 2. Tag for cloud registry
-docker tag chimera-akai:latest akai-hana/chimera-akai:latest
-
+docker tag chimera-akai:latest akaihanatsundeee/chimera-akai:latest
 # 3. Push to cloud
-docker push akai-hana/chimera-akai:latest
+docker push akaihanatsundeee/chimera-akai:latest
 
 # git sync
 git add .
