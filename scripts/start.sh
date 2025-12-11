53e79e4106be5e67bc4ae3dae15c053b7a6b5f03
$USER <akaihanatsundeee@proton.me>
2025-12-11 09:59:10 +0100

    automated sync

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 # open the host's x server to docker (ghostty, firefox, etc)
 xhost +local:docker
 docker compose up -d
-docker exec -u akai -it chimera-akai ghostty
+docker exec -u root -it chimera-akai ghostty
